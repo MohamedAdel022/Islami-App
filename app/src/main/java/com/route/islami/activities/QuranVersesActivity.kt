14dea7435fa7e0b2cfd90571f73705c020c07e28
Mohamed Adel <madel7586@gmail.com>
2025-08-05 00:24:46 +0300

    Refactor: Organize code into activities and fragments
    - Moved MainActivity, SplashIslamiActivity to `activities` package.
    - Created `QuranFragment` in `fragments.quran` package.
    - Added `QuranVersesActivity` for displaying Quran verses.
    - Implemented RecyclerView in `QuranFragment` to display list of Quran chapters using `ChaptersAdapter`.
    - Added data model `ChapterDM` for Quran chapters.
    - Created `QuranUtils` to provide Quran chapter data.
    - Added `ChapterKeys` for intent extras.
    - Added sdp and ssp libraries for responsive UI.
    - Updated layouts and themes.
    - Set QuranFragment as default fragment in MainActivity.
    - Added Quran chapters to assets folder

--- app/src/main/java/com/route/islami/activities/QuranVersesActivity.kt
+++ app/src/main/java/com/route/islami/activities/QuranVersesActivity.kt
@@ -3,19 +3,52 @@ package com.route.islami.activities
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.route.islami.R
+import com.route.islami.activities.adapters.VersesAdapter
+import com.route.islami.databinding.ActivityQuranVersesBinding
+import com.route.islami.fragments.quran.utils.ChapterKeys
 
 class QuranVersesActivity : AppCompatActivity() {
+    private var ChapterNameEnglish: String? = null
+    private var ChapterNameArabic: String? = null
+    private var ChapterNumber: Int = 0
+    private var chapterLength: Int = 0
+    private lateinit var binding: ActivityQuranVersesBinding
+
+    private lateinit var adapter: VersesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_quran_verses)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityQuranVersesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initArgs()
+        initViews()
+    }
+
+    fun initArgs() {
+        ChapterNameEnglish = intent.getStringExtra(ChapterKeys.chapterNameEnglish)
+        ChapterNameArabic = intent.getStringExtra(ChapterKeys.chapterNameArabic)
+        ChapterNumber = intent.getIntExtra(ChapterKeys.chapterNumber, 0)
+        chapterLength = intent.getIntExtra(ChapterKeys.chapterLength, 0)
+    }
+
+    fun initViews() {
+        binding.toolbarTitleEnglish.text = ChapterNameEnglish
+        binding.verseTitleArabic.text = ChapterNameArabic
+        binding.toolbarBackButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+        val list = readChapterContent()
+        adapter = VersesAdapter(list)
+        binding.versesRecyclerView.adapter = adapter
+
+
+    }
+
+    fun readChapterContent(): List<String> {
+        return assets.open("quran/${ChapterNumber}.txt").bufferedReader().use { reader ->
+            reader.readLines().filter {
+                it.isNotEmpty() && it.isNotBlank()
+            }
         }
     }
 }
--- app/src/main/java/com/route/islami/fragments/quran/utils/ChapterKeys.kt
+++ app/src/main/java/com/route/islami/fragments/quran/utils/ChapterKeys.kt
@@ -1,8 +1,9 @@
 package com.route.islami.fragments.quran.utils
 
 object ChapterKeys {
-    val chapterNumber = "chapter_number"
-    val chapterNameArabic = "chapter_name_arabic"
-    val chapterNameEnglish = "chapter_name_english"
-    val chapterLength = "chapter_length"
+    const val chapterNumber = "chapter_number"
+    const val chapterNameArabic = "chapter_name_arabic"
+    const val chapterNameEnglish = "chapter_name_english"
+    const val chapterLength = "chapter_length"
+    val chapterDM = "chapter_dm"
 }
